9357491f05d3e7b072817cf9969e408ff54262f8
okovalov <okovalov@snapchat.com>
2017-12-28 16:55:42 +0200

    formating shaders code

--- app/src/main/java/com/snap/lighting/SceneLightRenderer.java
+++ app/src/main/java/com/snap/lighting/SceneLightRenderer.java
@@ -15,40 +15,48 @@ public class SceneLightRenderer implements GLSurfaceView.Renderer {
 
     private static final int BYTES_PER_FLOAT = 4;
 
-    private static final String VERTEX_SHADER_CODE = "uniform mat4 u_modelViewProjectionMatrix;" +
+    private static final String VERTEX_SHADER_CODE =
+            "uniform mat4 u_modelViewProjectionMatrix;" +
+
             "attribute vec3 a_vertex;" +
             "attribute vec3 a_normal;" +
             "attribute vec4 a_color;" +
+
             "varying vec3 v_vertex;" +
             "varying vec3 v_normal;" +
             "varying vec4 v_color;" +
+
             "void main() {" +
-            "v_vertex=a_vertex;" +
-            "vec3 n_normal=normalize(a_normal);" +
-            "v_normal=n_normal;" +
-            "v_color=a_color;" +
-            "gl_Position = u_modelViewProjectionMatrix * vec4(a_vertex,1.0);" +
+                "v_vertex = a_vertex;" +
+                "vec3 n_normal = normalize(a_normal);" +
+                "v_normal = n_normal;" +
+                "v_color = a_color;" +
+                "gl_Position = u_modelViewProjectionMatrix * vec4(a_vertex, 1.0);" +
             "}";
 
-    private static final String FRAGMENT_SHADER_CODE = "precision mediump float;" +
+    private static final String FRAGMENT_SHADER_CODE =
+            "precision mediump float;" +
+
             "uniform vec3 u_camera;" +
             "uniform vec3 u_lightPosition;" +
+
             "varying vec3 v_vertex;" +
             "varying vec3 v_normal;" +
             "varying vec4 v_color;" +
+
             "void main() {" +
-            "vec3 n_normal=normalize(v_normal);" +
-            "vec3 lightvector = normalize(u_lightPosition - v_vertex);" +
-            "vec3 lookvector = normalize(u_camera - v_vertex);" +
-            "float ambient=0.2;" +
-            "float k_diffuse=0.3;" +
-            "float k_specular=0.5;" +
-            "float diffuse = k_diffuse * max(dot(n_normal, lightvector), 0.0);" +
-            "vec3 reflectvector = reflect(-lightvector, n_normal);" +
-            "float specular = k_specular * pow( max(dot(lookvector,reflectvector),0.0), 40.0 );" +
-            "vec4 one=vec4(1.0,1.0,1.0,1.0);" +
-            "vec4 lightColor = (ambient+diffuse+specular)*one;" +
-            "gl_FragColor = mix(lightColor,v_color,0.6);" +
+                "vec3 n_normal = normalize(v_normal);" +
+                "vec3 lightvector = normalize(u_lightPosition - v_vertex);" +
+                "vec3 lookvector = normalize(u_camera - v_vertex);" +
+                "float ambient = 0.2;" +
+                "float k_diffuse = 0.3;" +
+                "float k_specular = 0.5;" +
+                "float diffuse = k_diffuse * max(dot(n_normal, lightvector), 0.0);" +
+                "vec3 reflectvector = reflect(-lightvector, n_normal);" +
+                "float specular = k_specular * pow( max(dot(lookvector,reflectvector),0.0), 40.0 );" +
+                "vec4 one = vec4(1.0, 1.0, 1.0, 1.0);" +
+                "vec4 lightColor = (ambient+diffuse+specular)*one;" +
+                "gl_FragColor = mix(lightColor,v_color,0.6);" +
             "}";
 
     private float xСameraPosition, yCameraPosition, zCameraPosition;
