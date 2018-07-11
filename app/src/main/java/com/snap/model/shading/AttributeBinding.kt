2f59d012c7f3f827bccf3ab7d60f3af770b3ad6d
okovalov <okovalov@snapchat.com>
2018-07-11 12:38:20 -0700

    attribute pointer

--- app/src/main/java/com/snap/model/shading/AttributeBinding.kt
+++ app/src/main/java/com/snap/model/shading/AttributeBinding.kt
@@ -4,7 +4,14 @@ import android.opengl.GLES20
 
 import java.nio.FloatBuffer
 
-class AttributeBinding(programHandle: Int, val attributeName: String) {
+/**
+ * Each point in 3D scene has associated attributes with it - like:
+ * vertex position;
+ * vertex color;
+ * texture coordinates;
+ * normal direction;
+ */
+class AttributeBinding(programHandle: Int, attributeName: String) {
 
     private val attributeHandle: Int = GLES20.glGetAttribLocation(programHandle, attributeName)
 
@@ -13,8 +20,12 @@ class AttributeBinding(programHandle: Int, val attributeName: String) {
             sizePerItem: Int,
             isNormalized: Boolean = false,
             stride: Int = 0) {
-        GLES20.glEnableVertexAttribArray(attributeHandle)
+        enableVertexAttribute()
         GLES20.glVertexAttribPointer(attributeHandle, sizePerItem, GLES20.GL_FLOAT, isNormalized,
                 stride, floatBuffer)
+    }
+
+    private fun enableVertexAttribute() {
+        GLES20.glEnableVertexAttribArray(attributeHandle)
     }
 }
